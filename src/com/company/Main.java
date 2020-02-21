8cb1b5944826f457187cf2fe20031e2709196c8d
SunnyGare <shushikatom@gmail.com>
2020-02-21 19:34:31 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            System.out.println("Hello world 11");
+            System.out.println("Hello world");
         }
     }
 }
