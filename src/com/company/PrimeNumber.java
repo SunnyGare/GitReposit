c85fb020b249d80e47fb7dcf85821a81b3de4287
SunnyGare <shushikatom@gmail.com>
2020-03-01 22:24:39 +0200

    Prime Number

--- src/com/company/PrimeNumber.java
+++ src/com/company/PrimeNumber.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class PrimeNumber {
     public static boolean isSimpleNumber(int x) {
         int result = 0;
@@ -13,10 +18,46 @@ public class PrimeNumber {
     }
 
     public static void main(String[] args) {
-        boolean isSimle;
+        boolean isSimple;
         int x = 7;
-        isSimle = isSimpleNumber(x);
-        String answer = (isSimle != true) ? "No." : "Yes.";
+        isSimple = isSimpleNumber(x);
+        String answer = (isSimple != true) ? "No." : "Yes.";
         System.out.println("Is " + x + " prime number? - " + answer);
+    }
+
+    @Test
+    public  void testIsSimpleNumber (){
+        boolean simpleNumber = PrimeNumber.isSimpleNumber(7);
+        assertTrue (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(5);
+        assertTrue (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(30817);
+        assertTrue (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(33751);
+        assertTrue (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(34981);
+        assertTrue (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(61837);
+        assertTrue (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(64109);
+        assertTrue (simpleNumber);
+
+    }
+    @Test
+    public  void testIsSimpleNumberNegative (){
+        boolean simpleNumber = PrimeNumber.isSimpleNumber(8);
+        assertFalse (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(6);
+        assertFalse (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(30818);
+        assertFalse (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(33752);
+        assertFalse (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(34983);
+        assertFalse (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(61838);
+        assertFalse (simpleNumber);
+        simpleNumber = PrimeNumber.isSimpleNumber(64110);
+        assertFalse (simpleNumber);
     }
 }
