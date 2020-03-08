8b2eca08f705a0458d86c029bacfcffae781dcab
SunnyGare <shushikatom@gmail.com>
2020-03-08 09:34:28 +0200

    FactorialsAmount with minus in row

--- src/com/company/FactorialsAmount.java
+++ src/com/company/FactorialsAmount.java
@@ -7,9 +7,6 @@ import static org.junit.Assert.assertTrue;
 
 public class FactorialsAmount {
     public static int sumOfFactorials(int enteredNumber) {
-        //factorial(1) + factorial (2) + factorial (3) + factorial (4) + factorial (5)
-
-
         int sum = 0;
         for (int i = 1; i <= enteredNumber; i++) {
             sum += factorial(i);
@@ -17,67 +14,28 @@ public class FactorialsAmount {
         return sum;
     }
 
-  /*  public static int moduloValue(int enteredNumber) {
-        int[] negativeNumbers = new int[10];
-        int[] positiveNumbers = new int[10];
-        int result = 0;
-        for (int i = 1; i <= enteredNumber; i++) {
-            result = i % 2;
-            if (result >= 1) positiveNumbers[i] = factorial(i);
-            if (result == 0) negativeNumbers[i] = -factorial(i);
-        }
-        for (int j=0; j<negativeNumbers.length; j++){
-            System.out.println(negativeNumbers[j] + "     " + positiveNumbers[j]);
-        }
-        return negativeNumbers[4];
-    }*/
-
-
     public static int factorial(int x) {
         int result = 1;
+        int marker = x % 2;
         for (int i = 1; i <= x; i++) {
             result *= i;
         }
+        if (marker == 0) result = -result;
         return result;
     }
 
     public static void main(String[] args) {
         int some = sumOfFactorials(5);
         int some2 = factorial(5);
-       // int some3 = moduloValue (5);
-        System.out.print(some + "    " + some2/* + "    " + some3*/);
-    }
-
-    public static int sumOfFactorials2(int enteredNumber) {
-        int sum = 0;
-        for (int i = 1; i <= enteredNumber; i++) {
-            int result = 1;
-            int marker = 1;
-            marker = i%2;
-            //if(marker == 0) result = - result;
-           // if(marker != 0) sum = sum + result;
-            // System.out.print(result);
-            for (int j = 1; j <= i; j++) {
-                result *= j;
-            }
-        }
-        return sum;
+        System.out.print(some + "    " + some2);
     }
 
     @Test
     public void testIsSimpleNumber() {
         int sum = FactorialsAmount.sumOfFactorials(5);
-        assertEquals(153, sum);
+        assertEquals(101, sum);
         sum = FactorialsAmount.sumOfFactorials(4);
-        assertEquals(33, sum);
-    }
-
-    @Test
-    public void testIsSimpleNumber2() {
-        int sum = FactorialsAmount.sumOfFactorials2(5);
-        assertEquals(153, sum);
-        sum = FactorialsAmount.sumOfFactorials2(4);
-        assertEquals(33, sum);
+        assertEquals(-19, sum);
     }
 }
 
